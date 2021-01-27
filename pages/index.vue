0028b7e377787c694743de635c737379754b3164
cesar dv <cesardv@protonmail.ch>
2021-01-27 01:44:38 -0500

    add all files

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section tribute">
+      <h1 class="title">In memoriam</h1>
+      <img height="292" src="~/assets/frontimage.png" alt="In memoriam" />
+      <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/order-of-service.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <h1 class="title">Misa</h1>
+      <p><em>28 Enero 2021</em> Debido a la situación de COVID-19, servicios fúnebres están restringidos y no habrá reunión de misa por el momento.</p>      
+      <br>
+      <p>Por favor, cuídense mucho por ahora y en el futuro celebraremos la vida de Guido como se merece.<br>Mil gracias.</p>
+      
+      <h2>Servicio de Misa<span>[Futurahora]</span></h2>
+      <ul class="fa-ul">
+        <ListItem>Servicio comienza [Futuramente]</ListItem>
+      </ul>
+      <h2>Crematorio <span>3:00pm</span></h2>
+      <ul class="fa-ul">
+        <ListItem>Servicio privado antes a las 2pm at Los Olivos</ListItem>
+        <ListItem>Sus cenizas estaran acompañando la tumba de Betty en Santander de Quilichao.</ListItem>
+      </ul>
+      <p class="highlight">QED</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
